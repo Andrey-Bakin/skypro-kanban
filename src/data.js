c0cac21f64213f66a0a897f2a670de4f528532ba
Andrey Bakin <andybulkin@yandex.ru>
2024-03-22 16:46:05 +0300

    создан массив для карточек в файле data.js

---
+++ src/data.js
@@ -0,0 +1,81 @@
+export const cardList = [
+    {
+        id: 1,
+        topic: "Web Desing",
+        title: "Название задачи",
+        date: "30.10.23",
+        status: "Без статуса"
+    },
+    {
+        id: 2,
+        topic: "Research",
+        title: "Название задачи",
+        date: "30.10.23",
+        status: "Без статуса"
+    },
+    {
+        id: 3,
+        topic: "Web Desing",
+        title: "Название задачи",
+        date: "30.10.23",
+        status: "Без статуса"
+    },
+    {
+        id: 4,
+        topic: "Copywriting",
+        title: "Название задачи",
+        date: "30.10.23",
+        status: "Без статуса"
+    },
+    {
+        id: 5,
+        topic: "Web Desing",
+        title: "Название задачи",
+        date: "30.10.23",
+        status: "Без статуса"
+    },
+    {
+        id: 6,
+        topic: "Researc",
+        title: "Название задачи",
+        date: "30.10.23",
+        status: "Нужно сделать"
+    },
+    {
+        id: 7,
+        topic: "Research",
+        title: "Название задачи",
+        date: "30.10.23",
+        status: "В работе"
+    },
+    {
+        id: 8,
+        topic: "Copywriting",
+        title: "Название задачи",
+        date: "30.10.23",
+        status: "В работе"
+    },
+    {
+        id: 9,
+        topic: "Web Desing",
+        title: "Название задачи",
+        date: "30.10.23",
+        status: "В работеа"
+    },
+    {
+        id: 10,
+        topic: "Research",
+        title: "Название задачи",
+        date: "30.10.23",
+        status: "Тестирование"
+    },
+    {
+        id: 11,
+        topic: "Research",
+        title: "Название задачи",
+        date: "30.10.23",
+        status: "Готово"
+    },
+
+
+]
