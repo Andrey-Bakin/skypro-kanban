8f37fcc40cdfb4037d2b9106a777f98e6a6fad0f
Andrey Bakin <andybulkin@yandex.ru>
2024-03-23 15:01:22 +0300

    реализовано открытие/закрытие модального окна PopUser

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,11 @@
+import PopUser from "../popups/PopUser/PopUser"
+import { useState } from "react"
+
 function Header () {
+  const [isOpened, setIsOpened] = useState(false);
+  function togglePopUp() {
+    setIsOpened((isOpened) => !isOpened);
+  }
     return (<header className="header">
     <div className="container">
       <div className="header__block">
@@ -16,24 +23,10 @@ function Header () {
           <button className="header__btn-main-new _hover01" id="btnMainNew">
             <a href="#popNewCard">Создать новую задачу</a>
           </button>
-          <a href="#user-set-target" className="header__user _hover02">
+          <a href="#user-set-target" className="header__user _hover02" onClick={togglePopUp}>
             Ivan Ivanov
           </a>
-          <div
-            className="header__pop-user-set pop-user-set"
-            id="user-set-target"
-          >
-            {/* <a href="">x</a> */}
-            <p className="pop-user-set__name">Ivan Ivanov</p>
-            <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-            <div className="pop-user-set__theme">
-              <p>Темная тема</p>
-              <input type="checkbox" className="checkbox" name="checkbox" />
-            </div>
-            <button type="button" className="_hover03">
-              <a href="#popExit">Выйти</a>
-            </button>
-          </div>
+          {isOpened && <PopUser />}
         </nav>
       </div>
     </div>
